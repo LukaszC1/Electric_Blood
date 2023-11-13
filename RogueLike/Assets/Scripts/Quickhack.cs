2fcaa3b03b169122c379b456af2dd5fbb1f5e989
Mateusz Kies <lumios100@gmail.com>
2023-11-13 20:09:03 +0100

    quickhack synced

--- RogueLike/Assets/Scripts/Quickhack.cs
+++ RogueLike/Assets/Scripts/Quickhack.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
 public class Quickhack : WeaponBase
 {
@@ -12,39 +13,10 @@ public class Quickhack : WeaponBase
     }
 
     private IEnumerator SpawnHack()
-    {
-        //get enemy list somehow to fix the problem
-        List<GameObject> enemies = GetComponentInParent<WeaponManager>().enemiesManager.enemyList;
-
-
-        if (enemies.Count == 0) { yield break; }
-
+    {  
         for (int i = 0; i < weaponStats.amount; i++)
         {
-            GameObject hack = Instantiate(hackAnim);
-
-            Vector3 randomEnemy = enemies[UnityEngine.Random.Range(0, enemies.Count - 1)].transform.position;
-            if (randomEnemy == null) { yield break; }
-            hack.transform.position = randomEnemy;
-            hack.transform.localScale = new Vector2(hack.transform.localScale.x * transform.localScale.x, hack.transform.localScale.y * transform.localScale.y);
-
-            Collider2D[] colliders = Physics2D.OverlapCircleAll(hack.transform.position, weaponStats.size);
-            for (int j = 0; j < colliders.Length; j++)
-            {
-                iDamageable e = colliders[j].GetComponent<iDamageable>();
-                if (e != null)
-                {
-                    PostMessage((int)weaponStats.damage, colliders[j].transform.position);
-                    e.TakeDamage(weaponStats.damage);
-                }
-                Enemy enemy = colliders[j].GetComponent<Enemy>();
-                if (enemy != null)
-                {
-                    StartCoroutine(StunEnemy(enemy));
-                }
-            }
-
-
+            SpawnObjectServerRpc();
             yield return new WaitForSeconds(0.05f);
         }
     }
@@ -54,5 +26,37 @@ public class Quickhack : WeaponBase
         yield return new WaitForSeconds(0.8f);
         enemy.isStunned = false;
     }    
+
+    [ServerRpc]
+    private void SpawnObjectServerRpc()
+    {
+        List<GameObject> enemies = GetComponentInParent<WeaponManager>().enemiesManager.enemyList;
+
+        if (enemies.Count == 0 ) return;
+
+        GameObject hack = Instantiate(hackAnim);
+
+        Vector3 randomEnemy = enemies[UnityEngine.Random.Range(0, enemies.Count - 1)].transform.position;
+        if (randomEnemy == null) return;
+        hack.transform.position = randomEnemy;
+        hack.transform.localScale = new Vector2(hack.transform.localScale.x * transform.localScale.x, hack.transform.localScale.y * transform.localScale.y);
+        hack.GetComponent<NetworkObject>().Spawn();
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(hack.transform.position, weaponStats.size);
+        for (int j = 0; j < colliders.Length; j++)
+        {
+            iDamageable e = colliders[j].GetComponent<iDamageable>();
+            if (e != null)
+            {
+                PostMessage((int)weaponStats.damage, colliders[j].transform.position);
+                e.TakeDamage(weaponStats.damage);
+            }
+            Enemy enemy = colliders[j].GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                StartCoroutine(StunEnemy(enemy));
+            }
+        }
+    }
 
 }
